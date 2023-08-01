574c87a54b87e565b638cac33275ee933e244666
abu al ameen <abulyaqs@gmail.com>
2023-08-01 21:30:11 +0100

    all task

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+regex = /School/i
+
+if ARGV.empty?
+        puts "Usage: #{$PROGRAM_NAME} <string>"
+        exit 1
+end
+
+input_string = ARGV[0]
+
+matches = input_string.scan(regex)
+
+puts matches.join
