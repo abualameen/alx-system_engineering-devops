33962577fe442a62bd0d590866873004d46e6c43
abu al ameen <abulyaqs@gmail.com>
2023-09-24 18:55:32 +0100

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,8 @@
 # Puppet manifest to configure SSH client
-# Define a resource for the SSH client configuration file
 file_line { 'Configure SSH client':
-  path => '/home/abu/.ssh/config', 
-  line => [
-    'Host *',
-    'IdentityFile ~/.ssh/school',
-    'PasswordAuthentication no',
-  ],
+  path   => '/home/abu/.ssh/config',
+  line   => "Host *\nIdentityFile ~/.ssh/school\nPasswordAuthentication no",
+  ensure => present,
 }
+
 
