31d06ca7c1af0df310975336150d8fae744399f1
abu al ameen <abulyaqs@gmail.com>
2023-11-25 00:04:16 +0100

    task last

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,8 @@
-# Puppet manifest to kill a specific process using its PID
+# Puppet manifest to kill a specific process using its name
 
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
+exec { 'kill_killmennow':
+  command     => '/usr/bin/pkill killmennow',
   provider    => 'shell',
   returns     => [0, 1],
 }
-
 
