55470206c399bba8a6045695260b3969fc177da0
abu al ameen <abulyaqs@gmail.com>
2023-11-24 22:37:47 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,13 @@
-# Puppet manifest to install Flask from pip3
+# Puppet manifest to install Flask from pip3 with specific Python version
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-
+# Install the python3-pip package
+package { 'python3-pip':
+  ensure => installed,
 }
 
+# Install Flask and Werkzeug
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
