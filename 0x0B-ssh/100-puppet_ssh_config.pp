f615dbd3582a2151eb7bcd7f95a5881983c7a6a6
abu al ameen <abulyaqs@gmail.com>
2023-11-26 18:32:09 +0100

    all task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,12 @@
-# Import the ssh_config module
-include ssh_config::ssh_config
+# ssh_config.pp
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/abu/.ssh/config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/abu/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/id_rsa',
+}
+
