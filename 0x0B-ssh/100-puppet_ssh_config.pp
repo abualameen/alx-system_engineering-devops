737eb795b4e846c00b99700948c9ce9a5ef36f73
abu al ameen <abulyaqs@gmail.com>
2023-11-26 21:39:19 +0100

    now

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,17 @@
 # ssh_config.pp
 
-file { '/etc/ssh/sshd_config':
+file { '/home/abu/.ssh/config':
   ensure => present,
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
+  path  => '/home/abu/.ssh/config',
   line  => 'PasswordAuthentication no',
   match => '^#?PasswordAuthentication ',
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/sshd_config',
+  path  => '/home/abu/.ssh/config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#?IdentityFile ',
 }
