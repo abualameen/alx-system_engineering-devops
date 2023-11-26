9efa27ba5f047a3dfd309de17113b59e2dfa70ec
abu al ameen <abulyaqs@gmail.com>
2023-11-26 21:02:36 +0100

    noww

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,19 @@
 
 file { '/home/abu/.ssh/config':
   ensure  => present,
-  content => "Host *\n    IdentityFile ~/.ssh/id_rsa\nPasswordAuthentication no\n",
+  content => "Host *\n    IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
   mode    => '0600',
   owner   => 'abu',
   group   => 'abu',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/abu/.ssh/config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/abu/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
 }
 
