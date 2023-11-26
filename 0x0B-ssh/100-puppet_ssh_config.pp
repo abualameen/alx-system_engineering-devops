2ddcb8d75354cb8de070d51e9cc9c847223a6060
abu al ameen <abulyaqs@gmail.com>
2023-11-26 20:48:39 +0100

    now

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file { '/home/abu/.ssh/config':
   ensure  => present,
-  content => "Host *\n    IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
+  content => "Host *\n    IdentityFile ~/.ssh/id_rsa\nPasswordAuthentication no\n",
   mode    => '0600',
   owner   => 'abu',
   group   => 'abu',
