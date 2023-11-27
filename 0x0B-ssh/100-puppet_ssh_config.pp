1886324f60fcadd011cea2a6ee7bf02c3f906ecb
abu al ameen <abulyaqs@gmail.com>
2023-11-27 08:15:06 +0100

    correcton

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,13 @@
 # ssh_config.pp
 
-file { '/home/abu/.ssh/config':
-  ensure => present,
-}
-
 file_line { 'Turn off passwd auth':
-  path  => '/home/abu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^#?PasswordAuthentication ',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/abu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#?IdentityFile ',
 }
